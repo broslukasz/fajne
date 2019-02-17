6ed911717818bf1518c27ff001022aa12f4fa73d
lubros <lukasz.bros.sp@gmail.com>
2019-02-17 15:33:58 +0100

    ts mockito

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {CUSTOM_ELEMENTS_SCHEMA, Type} from "@angular/core";
+import { AngularFireDatabase } from "@angular/fire/database";
+import {anyString, anything, instance, mock, when} from "ts-mockito";
+import {Observable, of} from "rxjs";
+
+const mockProvider = <T>(
+  toMock: Type<T>,
+  setupMock: (m: T) => void
+): {
+  provide: Type<T>;
+  useFactory: () => T;
+} => {
+  const m = mock(toMock);
+  setupMock(m);
+  return { provide: toMock, useFactory: () => instance(m) };
+};
 
 describe('AppComponent', () => {
+  let angularFireDatabaseMock: AngularFireDatabase = mock(AngularFireDatabase);
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
+      providers: [
+        mockProvider(AngularFireDatabase, m =>
+          when(angularFireDatabaseMock.object(anyString())).thenReturn(<any>{
+            valueChanges(): Observable<any | null> {
+              return of(anything())
+            }
+          })
+        ),
+      ],
       declarations: [
         AppComponent
       ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
 
