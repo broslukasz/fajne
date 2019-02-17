2263175613da413979d3b07595c3cfd49f92ab25
lubros <lukasz.bros.sp@gmail.com>
2019-02-17 16:14:50 +0100

    login log out

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,14 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private user$: Observable<firebase.User | null>;
 
   constructor(
     private router: Router,
     private afAuth: AngularFireAuth
-  ) { }
+  ) {
+    this.user$ = this.afAuth.authState;
+  }
+
+  login(): void {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(() => this.router.navigate(['speach']))
+      .catch(error => console.log('auth error', error));
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+    this.router.navigate(['']);
+  }
 }
