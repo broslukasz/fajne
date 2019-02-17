b84b6fa9d2c22de05b07aad1e17d7792f745b282
lubros <lukasz.bros.sp@gmail.com>
2019-02-17 15:42:35 +0100

    app test initial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,37 +3,16 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import {CUSTOM_ELEMENTS_SCHEMA, Type} from "@angular/core";
 import { AngularFireDatabase } from "@angular/fire/database";
-import {anyString, anything, instance, mock, when} from "ts-mockito";
-import {Observable, of} from "rxjs";
-
-const mockProvider = <T>(
-  toMock: Type<T>,
-  setupMock: (m: T) => void
-): {
-  provide: Type<T>;
-  useFactory: () => T;
-} => {
-  const m = mock(toMock);
-  setupMock(m);
-  return { provide: toMock, useFactory: () => instance(m) };
-};
+import {instance, mock} from "ts-mockito";
 
 describe('AppComponent', () => {
-  let angularFireDatabaseMock: AngularFireDatabase = mock(AngularFireDatabase);
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       providers: [
-        mockProvider(AngularFireDatabase, m =>
-          when(angularFireDatabaseMock.object(anyString())).thenReturn(<any>{
-            valueChanges(): Observable<any | null> {
-              return of(anything())
-            }
-          })
-        ),
+        {provide: AngularFireDatabase, useValue: instance(mock(AngularFireDatabase))}
       ],
       declarations: [
         AppComponent
@@ -46,18 +25,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'fajne'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fajne');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to fajne!');
   });
 });
