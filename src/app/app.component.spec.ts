c669b09c8e5b97f1fc42f6dae23ac85e431bcb45
lubros <lukasz.bros.sp@gmail.com>
2019-03-09 17:26:27 +0100

    app component cleared

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,8 +2,6 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { instance, mock } from 'ts-mockito';
-import { FirabaseStateCommunicationService } from './core/firabase-state-communication.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,9 +9,7 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule
       ],
-      providers: [
-        {provide: FirabaseStateCommunicationService, useValue: instance(mock(FirabaseStateCommunicationService))}
-      ],
+      providers: [],
       declarations: [
         AppComponent
       ],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FirabaseStateCommunicationService } from './core/firabase-state-communication.service';
-import { AppStateComponent } from './core/app-state/app-state.component';
-import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent extends AppStateComponent implements OnInit {
-  public isResultAvailable: boolean;
-  public result: number;
+export class AppComponent implements OnInit {
+  constructor() {}
 
-  constructor(
-    public firabaseStateCommunicationService: FirabaseStateCommunicationService
-  ) {
-    super(firabaseStateCommunicationService);
-  }
-
-  public ngOnInit(): void {
-    this.watchForResultAvailbility();
-  }
-
-  private watchForResultAvailbility(): void {
-    this.firabaseStateCommunicationService.isResultAvailable$.subscribe((isResultAvailable: boolean) => {
-      this.isResultAvailable = isResultAvailable;
-
-      if (isResultAvailable) {
-        this.result = _.cloneDeep(this.actionCounter.getValue());
-      }
-    });
-  }
+  public ngOnInit(): void { }
 }
